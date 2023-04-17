50565b8bdeb0bd72a50bb6d878c3b30ea803bd51
matheus1964 <mateusaragao1@hotmail.com>
2023-04-17 14:44:59 -0300

    Correção feitas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import AppRoutes from './Routes/Routes'
 import './App.css';
-import Footer from './components/Footer';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Contato from './pages/Contato.js';
 
 function App() {
   return (
