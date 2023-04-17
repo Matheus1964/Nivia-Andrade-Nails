44d5fe00df0a639ae05c20e2b62c01a6c5281fbe
Taiyo <taiyo@unipam.edu.br>
2023-04-17 11:14:04 -0300

    Criação da página de contato

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-
 import './App.css';
 import Footer from './components/Footer';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Contato from './pages/Contato.js';
 
 function App() {
   return (
+    
     <div className="App">
-      <h1 className="teste">
-        nivia
-      </h1>
-
       
-      <Footer/>
+      <div className="container">
+
+        {/*Rota para a página de contato*/}
+        <Router>
+          <ul>
+            <li>
+              <Link to="/contato">Contato</Link>
+            </li>
+            
+          </ul>
+
+          <Routes>
+            <Route exact path="/contato" element={<Contato/>}/>
+          </Routes>
+
+        </Router>
+          {/*
+          <h1 className="teste">
+            nivia
+          </h1>
+          */}
+        
+      </div>
     </div>
   );
 }
