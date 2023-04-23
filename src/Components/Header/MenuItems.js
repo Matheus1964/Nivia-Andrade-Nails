839af7f95248473715790d6647b27ec48f5e5029
GabrielSanic <ghenrique018@gmail.com>
2023-04-23 15:03:50 -0300

    Header parcialmente completo

---
+++ src/Components/Header/MenuItems.js
@@ -0,0 +1,13 @@
+export const MenuItems = [
+  {
+    title: 'Modelos de 1,7cm',
+    path: '/modelo-1.7',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Modelos de 2cm',
+    path: '/modelo-2',
+    cName: 'dropdown-link'
+  }
+
+];
