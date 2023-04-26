ddaeb075e647a99042080241191a1a944e466518
matheus1964 <mateusaragao1@hotmail.com>
2023-04-26 01:00:14 -0300

    components

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,11 +1,15 @@
-import "../../Components/DescricaodoProduto/DescricaodoProduto.css"
-import Descricao from "../../Components/DescricaodoProduto/index"
-import "./Login.css"
+import style from "./Login.module.css";
+import "./Login.module.css"
 function Login(){
   return(
-    <div>
-      <Descricao/>
+    <form>
+    <div className={style.Container}>
+      <h1 className={style.TituloLogin}>Login</h1>
+      <input className={style.LoginInput} type="Email" name="name" placeholder="Username" maxlength="35" minlength="2" required></input>
+      <input className={style.LoginInput} type="Password" name="Password" placeholder="Password" maxlength="10" minlength="2" required></input>
+      <button className={style.LoginButton} type='submit' value='Send' autoFocus>Login</button>
     </div>
+    </form>
   )
 }
 export default Login;
