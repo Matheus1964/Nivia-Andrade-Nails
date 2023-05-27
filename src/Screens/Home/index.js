e0c33d4ea31c459950c1379f8333c0cb0ae123ea
Taiyo <taiyo@unipam.edu.br>
2023-05-27 19:58:03 -0300

    Modal de confirmação de pedido feito

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ProductList from '../../Components/ProductList/index.js';
 import './Home.css'
 import '../../Components/ProductList/ProductList.css'
+import ReactModal from '../../Components/Modal';
+import { useState } from 'react';
+
 function Home() {
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  //adaptar função que será chamada se o botão Finalizar for clicado
+  function clickFinalizar(){
+    console.log("Finalizado")
+  }
+
   return (
        <div className="home">
         <h1 className="titulo">Home</h1>
         <div className="div-row">
-          <ProductList/>
+          {/*<ProductList/>*/}
           {/*<div className="div-exemplo"></div>*/}
         </div>
         
-
+        {/*botão para teste do modal*/}
+        <button onClick={() => setIsOpen(true)}>Abrir modal</button>
+        <ReactModal show={modalIsOpen} setShow={setIsOpen} handleConfirmation={clickFinalizar}></ReactModal>
+        
         
        </div>
        
