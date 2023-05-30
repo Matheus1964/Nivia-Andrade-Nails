7f68c229e29f5fa5c01d97850f3feae10d585c2b
GabrielSanic <ghenrique018@gmail.com>
2023-05-30 17:32:09 -0300

    Mulher da home parcialmente completo

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Dropdown from './Dropdown';
 
-function Navbar() {
+function Navbar({ isHomePage }) {
   const [click, setClick] = useState(false);
   const [dropdown, setDropdown] = useState(false);
-  const [navHeight, setNavHeight] = useState(0);
+  const [transparent, setTransparent] = useState(isHomePage);
   const [produtosActive, setProdutosActive] = useState(false);
 
   const handleClick = () => setClick(!click);
@@ -28,23 +28,22 @@ function Navbar() {
     }
   };
 
+  // Deixa a navbar transparente na posição inicial
   useEffect(() => {
-    setNavHeight(document.querySelector('.navbar').offsetHeight);
-  }, []);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setNavHeight(document.querySelector('.navbar').offsetHeight);
+    const handleScroll = () => {
+      const currentPosition = window.scrollY;
+      if (isHomePage) {
+        setTransparent(currentPosition === 0);
+      }
     };
-    window.addEventListener('resize', handleResize);
+
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
 
-  useEffect(() => {
-    document.body.style.paddingTop = navHeight + 'px';
-  }, [navHeight]);
+  }, [isHomePage]);
 
   const toggleDropdown = () => {
     setDropdown(!dropdown);
@@ -57,44 +56,44 @@ function Navbar() {
 
   return (
     <>
-      <nav className='navbar'>
-        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+      <nav className={`navbar ${transparent && isHomePage ? 'transparent' : ''} ${isHomePage ? 'home' : ''}`}>
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
           Nivia Andrade Naíls
         </Link>
-        <div className='menu-icon' onClick={handleClick}>
+        <div className="menu-icon" onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Início
             </Link>
           </li>
           <li
-            className='nav-item'
+            className="nav-item"
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
             onClick={toggleDropdown}
           >
-            <Link to='#' className='nav-links'>
-              Produtos <i className='fas fa-caret-down' />
+            <Link to="#" className="nav-links">
+              Produtos <i className="fas fa-caret-down" />
             </Link>
             {dropdown && (
               <Dropdown
                 produtosActive={produtosActive}
                 handleProdutosClick={handleProdutosClick}
-                closeMobileMenu={closeMobileMenu} 
+                closeMobileMenu={closeMobileMenu}
               />
             )}
           </li>
-          <li className={`nav-item ${dropdown && "dropdown-active"}`}>
-            <Link to='/sobre' className='nav-links' onClick={closeMobileMenu}>
-              Sobre Nós
+          <li className={`nav-item ${dropdown && 'dropdown-active'}`}>
+            <Link to="/contato" className="nav-links" onClick={closeMobileMenu}>
+              Contato
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='/contato' className='nav-links' onClick={closeMobileMenu}>
-              Contato
+          <li className="nav-item">
+            <Link to="/sobre" className="nav-links" onClick={closeMobileMenu}>
+              Sobre
             </Link>
           </li>
         </ul>
