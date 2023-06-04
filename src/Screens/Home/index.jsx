d1b5b4663de9b8d53c17c0ad55de320624fade89
matheus1964 <mateusaragao1@hotmail.com>
2023-06-04 15:07:11 -0300

    Co-authored-by: Victor Pereira <dvictorps@users.noreply.github.com>

---
+++ src/Screens/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+       <div className="home">
+        <h1 className="titulo">Home</h1>
+       
+       
+       </div>
+  );
+}
+
+export default Home;
