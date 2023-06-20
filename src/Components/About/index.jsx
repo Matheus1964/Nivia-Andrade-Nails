0369da7d157c6d3de82adf2f98065298b4fb50b4
matheus1964 <mateusaragao1@hotmail.com>
2023-06-20 17:33:19 -0300

    textos feitos

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -12,8 +12,23 @@ function About () {
       <div className={styled.About}>
         <h1>Sobre</h1>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sed at sit necessitatibus dolores quidem aut, quod recusandae enim. Enim iure alias, numquam unde obcaecati error ipsam natus. Voluptate, tempore.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sed at sit necessitatibus dolores quidem aut, quod recusandae enim. Enim iure alias, numquam unde obcaecati error ipsam natus. Voluptate, tempore.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sed at sit necessitatibus dolores quidem aut, quod recusandae enim. Enim iure alias, numquam unde obcaecati error ipsam natus. Voluptate, tempore.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sed at sit necessitatibus dolores quidem aut, quod recusandae enim. Enim iure alias, numquam unde obcaecati error ipsam natus. Voluptate, tempore.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sed at sit necessitatibus dolores quidem aut, quod recusandae enim. Enim iure alias, numquam unde obcaecati error ipsam natus. Voluptate, tempore.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sed at sit necessitatibus dolores quidem aut, quod recusandae enim. Enim iure alias, numquam unde obcaecati error ipsam natus. Voluptate, tempore.
+        As películas de água de contorno são uma opção incrível para quem deseja adicionar designs detalhados e elegantes às unhas. Esses adesivos são produzidos em um filme especial que pode ser facilmente transferido para as unhas usando água. No nosso site, oferecemos uma ampla variedade de modelos de películas de água de contorno, permitindo que você encontre o estilo perfeito para suas unhas.
+
+Nossa coleção de modelos abrange diversos temas e estilos, garantindo opções para todos os gostos e ocasiões. Aqui estão algumas das categorias disponíveis em nosso site:
+
+Flores e plantas: Explore nossa seleção de películas de água com designs florais encantadores. De rosas delicadas a lírios elegantes, você encontrará uma variedade de opções para criar um visual romântico e feminino.
+
+Padrões geométricos: Para um toque moderno e estilizado, nossos modelos com padrões geométricos são ideais. Linhas precisas, formas abstratas e combinações de cores criativas proporcionarão um visual contemporâneo às suas unhas.
+
+Animal print: Se você está em busca de um estilo audacioso e ousado, confira nossa coleção de películas de água com estampas de animais, como leopardo, zebra ou cobra. Esses designs adicionam um toque selvagem às suas unhas.
+
+Temas sazonais: Para se adequar a cada estação do ano, oferecemos películas de água com temas sazonais. De flores primaveris a flocos de neve no inverno, você poderá atualizar suas unhas de acordo com o clima e as festividades.
+
+Personagens e símbolos: Seja um fã de desenhos animados, filmes ou ícones da cultura pop, temos modelos com personagens e símbolos populares. Mostre sua personalidade e interesse adicionando um toque divertido às suas unhas.
+
+Arte abstrata: Nossa coleção de modelos abstratos oferece uma variedade de opções criativas. Com pinceladas de tinta, padrões psicodélicos e formas abstratas, você pode criar um visual artístico e único nas suas unhas.
+
+Navegue em nosso site para descobrir a variedade completa de modelos de películas de água de contorno para adesivos de unha. Temos certeza de que você encontrará designs que combinam com seu estilo e tornarão suas unhas verdadeiras obras de arte. Aplique nossas películas com facilidade e encante a todos com unhas deslumbrantes. Explore nossa coleção agora e transforme sua manicure em algo especial!
         </p>
       </div>
     </div>
--- src/Components/DescricaodoProduto/index.jsx
+++ src/Components/DescricaodoProduto/index.jsx
@@ -9,7 +9,7 @@ function Descricao(){
         <h1 className={style.Titulo_produto}>Descrição do produto</h1>
       </div>
       <div className={style.DescricaoTexto}>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eum illo adipisci minus quidem ad maiores sint aperiam error assumenda aspernatur similique aut consectetur voluptate neque recusandae ex, soluta hic saepe corrupti omnis atque. Ipsam quasi maxime ab nisi corrupti quod, voluptatem fugit temporibus saepe harum, sint repellat id doloribus expedita in. Quasi laudantium aut numquam magnam autem quae iusto consequuntur, corrupti reiciendis nisi, quod, vero unde illum totam eaque accusantium placeat! Voluptatibus nihil earum ipsa at aliquid autem enim dignissimos non dicta asperiores inventore maxime repudiandae, dolorem consequuntur, unde quibusdam sunt nam libero, aliquam incidunt nulla! Vero, maiores cumque!</p>
+        <p>As películas de água de contorno são uma opção popular para a produção de adesivos de unha com designs detalhados. Esses adesivos são impressos em um filme especial que pode ser transferido para as unhas usando água. Existem vários modelos disponíveis, incluindo flores, padrões geométricos, estampas de animais, temas sazonais, personagens e símbolos, além de arte abstrata. A variedade de designs pode variar dependendo da marca e do fornecedor.</p>
       </div>
     </div>
   );
